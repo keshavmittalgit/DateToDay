24d6546702bd49ab2a52dd0d57ae42ae218c80ff
keshavmittalgit <keshavmittal7974@gmail.com>
2024-02-03 19:09:31 +0530

    number of days in a month funciton is implemented

--- test.cpp
+++ test.cpp
@@ -1,34 +1,54 @@
-
 #include <bits/stdc++.h>
-
 using namespace std;
 
+
+
+// calculate the days in months;
+int NumberOfDaysInMonth(int month){
+    int numberOfDays =0;
+    for (int i = 1; i < month; i++)
+    {   
+        if(i==2){
+            numberOfDays+= 28;
+            continue;
+        }
+        if(i<8){
+            if(i%2==0){
+                numberOfDays+=30;
+            }
+            else numberOfDays+=31;
+        }
+        else {
+            if(i%2==0){
+                numberOfDays+=31;
+            }
+            else numberOfDays+=30;
+        }
+    }
+    return numberOfDays;
+}
+
 // calculate the leap years;
-int calLeapYear(int n){
-   return (n / 4) - (n / 100) + (n / 400); 
+int calLeapYear(int year){
+   return (year / 4) - (year / 100) + (year / 400); 
 }
+
 // calculate the normal years;
-int calNormalYear(int n){
-    return n - calLeapYear(n);
+int calNormalYear(int year){
+    return year - calLeapYear(year);
 }
+
 // year deviatation;
 int deviatation(int year){
-
-    
     int d = (calLeapYear(year)*2 + calNormalYear(year)+5)%7;
     if(year%4 == 0) {
         d--;
     }
-
-
-
-
-    
     return d;
 }
 
 int main(){
-    int year = 5;
-    cout << deviatation(year) << endl;
+    int test = 3;
+    cout << NumberOfDaysInMonth(test) << endl;
     return 0;
 }
